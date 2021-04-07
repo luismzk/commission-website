c1f1fd59c2beec0b9dabaec0d980716fbd60f7ca
Luis I. Martinez K <luismzk@gmail.com>
2021-04-07 01:39:05 -0400

    lots of landing page changes

--- routes/landing/landing.js
+++ routes/landing/landing.js
@@ -6,13 +6,15 @@ const { getUsers } = require("./utilities")
 // Landing Page
 router.get('/', async (req, res) => {
 
-	try {
+	/*try {
 		let users = await getUsers();
 	  return res.send(users)
 	} catch(e){
 		console.log(e)
 		return res.sendStatus(500)
-	}
+	}*/
+
+	return res.render("landing/landing")
 
 });
 
